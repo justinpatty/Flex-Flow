89d6d11d48b03489a39647f595c880fb921ba68d
Conor <conor.reed@gmail.com>
2023-11-01 13:01:08 -0500

    created the models for cardio, weight training, and log. Also added basic fields to the models

---
+++ models/cardio.js
@@ -0,0 +1,29 @@
+// models/Cardio.js
+
+module.exports = (sequelize, DataTypes) => {
+    const Cardio = sequelize.define('Cardio', {
+      title: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      description: {
+        type: DataTypes.TEXT,
+        allowNull: true,
+      },
+      duration: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+      },
+      distance: {
+        type: DataTypes.FLOAT,
+        allowNull: false,
+      },
+    });
+  
+    Cardio.associate = (models) => {
+      // Define associations here (if any)
+    };
+  
+    return Cardio;
+  };
+  
