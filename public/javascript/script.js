58a0868cd102069c51397da311b74afbb1048734
CRich96 <you@example.comchris575502@icloud.com>
2023-11-02 13:13:34 -0500

    handlebar update

--- public/javascript/script.js
+++ public/javascript/script.js
@@ -1,25 +1,17 @@
-function startClock() {
-    const hourHand = document.querySelector(".hour-hand");
-    const minuteHand = document.querySelector(".minute-hand");
-    const secondHand = document.querySelector(".second-hand");
-
-    function updateClock() {
-        const now = new Date();
-        const hours = now.getHours();
-        const minutes = now.getMinutes();
-        const seconds = now.getSeconds();
-
-        const hourRotation = (360 / 12) * hours + (360 / 12) * (minutes / 60);
-        const minuteRotation = (360 / 60) * minutes + (360 / 60) * (seconds / 60);
-        const secondRotation = (360 / 60) * seconds;
-
-        hourHand.style.transform = `rotate(${hourRotation}deg)`;
-        minuteHand.style.transform = `rotate(${minuteRotation}deg)`;
-        secondHand.style.transform = `rotate(${secondRotation}deg)`;
-    }
-
-    updateClock();
-    setInterval(updateClock, 1000);
+function updateClock() {
+    const now = new Date();
+    const hours = String(now.getHours()).padStart(2, '0');
+    const minutes = String(now.getMinutes()).padStart(2, '0');
+    const seconds = String(now.getSeconds()).padStart(2, '0');
+    
+    const timeString = `${hours}:${minutes}:${seconds}`;
+    
+    const clockElement = document.getElementById('clock');
+    clockElement.textContent = timeString;
 }
 
-window.addEventListener("DOMContentLoaded", startClock);
+// Update the clock every second
+setInterval(updateClock, 1000);
+
+// Initial call to display the clock immediately
+updateClock();
