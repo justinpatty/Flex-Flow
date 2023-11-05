fae4ff0918e04b06341504ddb0cabbb508fb0fec
Justin Patty <justinpatty54@gmail.com>
2023-11-05 17:19:26 -0600

    merging

--- test/api-test.js
+++ test/api-test.js
@@ -1,14 +1,10 @@
+const request = require("request");
 
-var muscle = 'biceps'
-$.ajax({
-    method: 'GET',
-    url: 'https://api.api-ninjas.com/v1/exercises?muscle=' + muscle,
-    headers: { 'X-Api-Key': 'wP5vqzkikC7Gw3IK1nXbDg==bQIOoLezyk0YDbm4'},
-    contentType: 'application/json',
-    success: function(result) {
-        console.log(result);
-    },
-    error: function ajaxError(jqXHR) {
-        console.error('Error: ', jqXHR.responseText);
-    }
-});
+function workout() {
+    
+        url: "https://api.api-ninjas.com/v1/exercises?",
+        headers: {
+            "X-Api-Key": "wP5vqzkikC7Gw3IK1nXbDg==bQIOoLezyk0YDbm4",
+        },
+
+};
