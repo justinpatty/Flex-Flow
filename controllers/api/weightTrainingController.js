e8de419d37068e6248f5fa0e3e8cdcede9d6ac5e
CRich96 <you@example.comchris575502@icloud.com>
2023-11-05 17:14:39 -0600

    current changes

--- controllers/api/weightTrainingController.js
+++ controllers/api/weightTrainingController.js
@@ -6,7 +6,7 @@ const getWeightTrainings = async (req, res) => {
   } catch (error) {
     res.status(500).json({ error: 'Internal server error' });
   }
-})
+}
 
 router.post("/", async (req, res) => {
   const { title, description, reps, sets } = req.body;
