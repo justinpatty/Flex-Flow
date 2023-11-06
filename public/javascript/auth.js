5267dc696170e60567d33e100bed7b1262611bcb
Conor <conor.reed@gmail.com>
2023-11-06 10:11:57 -0600

    fix stuff

--- public/javascript/auth.js
+++ public/javascript/auth.js
@@ -1,24 +1,24 @@
 // public/assets/js/auth.js
 
 document.addEventListener('DOMContentLoaded', () => {
-    const loginForm = document.getElementById('login-form');
-    
+    const loginForm = document.getElementById('loginForm');
+    console.log("test")
     if (loginForm) {
         loginForm.addEventListener('submit', (event) => {
             event.preventDefault();
             
-            const email = document.getElementById('email').value;
+            const username = document.getElementById('username').value;
             const password = document.getElementById('password').value;
 
             // Implement form validation here, e.g., check if email and password are valid
 
             // Send login data to the server
-            fetch('/login', {
+            fetch('/api/user/login', {
                 method: 'POST',
                 headers: {
                     'Content-Type': 'application/json',
                 },
-                body: JSON.stringify({ email, password }),
+                body: JSON.stringify({ username, password }),
             })
             .then((response) => {
                 if (response.status === 200) {
@@ -35,3 +35,29 @@ document.addEventListener('DOMContentLoaded', () => {
         });
     }
 });
+
+const signupFormHandler = async (event) => {
+    event.preventDefault();
+  
+    const username = document.querySelector('#newUsername').value.trim();
+    const password = document.querySelector('#newPassword').value.trim();
+  
+    if (username && password) {
+      const response = await fetch('/api/user', {
+        method: 'POST',
+        body: JSON.stringify({ username, password }),
+        headers: { 'Content-Type': 'application/json' },
+      });
+  
+      if (response.ok) {
+        document.location.replace('/profile');
+      } else {
+        alert(response.statusText);
+      }
+    }
+  };
+
+  document
+  .querySelector('#signupForm')
+  .addEventListener('submit', signupFormHandler);
+
